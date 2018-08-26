d9cbbfcab491b27d92ac2c82b36e4ca00dde91a0
Dayi Lin <heylindayi@gmail.com>
2018-08-26 04:36:00 -0400

    project

--- hackathon_detail.php
+++ hackathon_detail.php
@@ -109,7 +109,7 @@
 							</p>
 <?php
 
-    $sql = "SELECT * FROM project WHERE hackathon_alias = '".$alias."' ORDER BY predict DESC";
+    $sql = "SELECT * FROM project WHERE hackathon_alias = '".$alias."' ORDER BY win, predict DESC";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
